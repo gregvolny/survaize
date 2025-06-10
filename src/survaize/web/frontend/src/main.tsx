e676f02457eccb86aedd08e921748a42df7f8cee
Josh Handley <josh@teleyah.com>
2025-06-10 17:11:06 -0400

    Fix import issues

--- src/survaize/web/frontend/src/main.tsx
+++ src/survaize/web/frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "@uiw/react-codemirror/dist/codemirror.css";
 
 const rootElement = document.getElementById("root");
 
